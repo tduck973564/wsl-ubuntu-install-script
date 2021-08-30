7ca1205e8c564eb795cd388833c618bb5b99f031
Thomas <tduck973564@gmail.com>
2021-08-30 18:41:30 +1000

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,4 +1,5 @@
 sudo apt update
+sudo apt install python-is-python3
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 sudo apt install python3 python3-pip build-essential git
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
